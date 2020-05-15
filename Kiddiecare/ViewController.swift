91a31ca0ea0195b42089e9d8316a1028ad009dfb
saranya <saranpisces19@gmail.com>
2020-05-15 09:37:33 +0530

    first commit

--- Kiddiecare/ViewController.swift
+++ Kiddiecare/ViewController.swift
@@ -5,14 +5,28 @@
 //  Created by Saranya ramamoorthy on 11/05/20.
 //  Copyright © 2020 Saranya ramamoorthy. All rights reserved.
 //
-
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate {
 
+    @IBOutlet weak var webview: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL (string: "https://reachwebdemo.com/2020/02/kiddiecare/")
+               let requestObj = URLRequest(url: url!)
+               webview.navigationDelegate = self
+               webview.load(requestObj)
+               webview.allowsBackForwardNavigationGestures = true
+      
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webview.frame.size.height = 1
+        webView.frame.size = webView.sizeThatFits(CGSize.zero)
     }
 
 
